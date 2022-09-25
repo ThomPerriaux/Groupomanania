18752514cf41a2d0c0edeb28f88c954988e2c938
thomas perriaux <thomas.perriaux@gmail.com>
2022-09-25 16:02:23 +0200

    cleanCodeRoutes

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,12 @@
 const express = require ('express');
-const router = express.Router();
 const postControl = require('../controllers/post.controller.js');
 const auth = require('../middleware/auth.middleware');
-const multer = require('multer');
+
 const upload = multer();
+const router = express.Router();
 
 // '/' => /api/profile
-router.post('/', auth, upload.single("picture"), postControl.createPost);
+router.post('/', auth, upload.single("picture"), postControl.createPost); //ulpoad
 router.post('/:id/like', auth, postControl.likePost);
 
 router.get('/', auth , postControl.getAllPosts);
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+
 const router = express.Router();
 const userControl = require('../controllers/user.controller.js');
 
